3f738a1f6b0a92abc48ec0527a466fcd7227b3c4
Travis Jefferson <tjefferson@signpost.com>
2015-08-22 16:44:32 -0500

    indentation cleanup

--- test/fogbugz-test.el
+++ test/fogbugz-test.el
@@ -17,44 +17,47 @@
 ;; `fogbugz-logon'
 (ert-deftest fogbugz-logon-request-test ()
   (with-mock
-    (mock (fogbugz-request
-           "logon"
-           '(("email" . "email@email.com") ("password" . "secret")))
-          => "raw-data")
-    (mock (fogbugz-parse-logon-response "raw-data") => "parsed-token")
+   (mock (fogbugz-request
+          "logon"
+          '(("email" . "email@email.com") ("password" . "secret")))
+         => "raw-data")
+   (mock (fogbugz-parse-logon-response "raw-data") => "parsed-token")
 
-    (let ((fogbugz-token nil)
-          (fogbugz-email "email@email.com")
-          (fogbugz-password "secret"))
-      (should (equal (fogbugz-logon) "parsed-token")))))
+   (let ((fogbugz-token nil)
+         (fogbugz-email "email@email.com")
+         (fogbugz-password "secret"))
+     (should (equal (fogbugz-logon) "parsed-token")))))
 
 (ert-deftest fogbugz-logon-no-request-test ()
   (with-mock
-    (not-called fogbugz-request)
-    (not-called fogbugz-parse-logon-response)
+   (not-called fogbugz-request)
+   (not-called fogbugz-parse-logon-response)
 
-    (let ((fogbugz-token "already-set"))
-      (should (equal (fogbugz-logon) "already-set")))))
+   (let ((fogbugz-token "already-set"))
+     (should (equal (fogbugz-logon) "already-set")))))
 
 ;; `fogbugz-request'
 (ert-deftest fogbugz-request-exclude-token-test ()
   (with-mock
-    (let ((xml-buffer (generate-new-buffer "mock-xml-buffer"))
+   (let ((xml-buffer (generate-new-buffer "mock-xml-buffer"))
 
-          ;; fake headers end at char 13 in xml data
-          (url-http-end-of-headers 13))
+         ;; fake headers end at char 13 in xml data
+         (url-http-end-of-headers 13))
 
-      (save-excursion
-        (switch-to-buffer xml-buffer)
-        (insert "FAKE HEADERS<?xml version=\"1.0\" encoding=\"UTF-8\"?><response><data>123</data></response>"))
+     (save-excursion
+       (switch-to-buffer xml-buffer)
+       (insert "FAKE HEADERS"
+               "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+               "<response><data>123</data></response>"))
 
-      (mock (url-retrieve-synchronously "<mock url>") => xml-buffer)
-      (mock (fogbugz-base-uri "endpoint-name") => "<mock url>")
-      (should (equal
-               (fogbugz-request "endpoint-name" `(("param1" . "value1") ("param2" . "value2")) t)
+     (mock (url-retrieve-synchronously "<mock url>") => xml-buffer)
+     (mock (fogbugz-base-uri "endpoint-name") => "<mock url>")
+     (should (equal
+              (fogbugz-request "endpoint-name" `(("param1" . "value1") ("param2" . "value2")) t)
 
-               ;; mock xml parsed into lisp object
-               '(response nil (data nil "123")))))))
+              ;; mock xml parsed into lisp object
+              '(response nil (data nil "123")))))))
+
 ;; `fogbugz-search'
 (ert-deftest fogbugz-search-no-params ()
   (with-mock
